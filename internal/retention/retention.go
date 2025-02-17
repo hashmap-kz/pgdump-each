ce2005234a311201d6fc30f22e1823e025e07079
alexey.zh <mailbox.stdio@gmail.com>
2025-02-17 15:07:34 +0500

    retention - retention routine

--- internal/retention/retention.go
+++ internal/retention/retention.go
@@ -2,51 +2,151 @@ package retention
 
 import (
 	"fmt"
+	"log/slog"
 	"os"
 	"path/filepath"
+	"sort"
 	"time"
 
 	"gopgdump/config"
 	"gopgdump/internal/naming"
 )
 
+type retainInfo struct {
+	// path info
+	absPath  string
+	path     string
+	basename string
+
+	// parsed path meta-info
+	datetime time.Time
+	host     string
+	port     string
+	dbname   string
+}
+
+// host+port+dbname=[]backups
+type retainList map[string][]retainInfo
+
 func PurgeOldDirs() error {
 	cfg := config.Cfg()
 	if !cfg.Retention.Enable {
 		return nil
 	}
-	_, err := time.ParseDuration(cfg.Retention.Period)
+
+	retentionPeriod, err := time.ParseDuration(cfg.Retention.Period)
 	if err != nil {
 		return err
 	}
 
-	dirs, err := findDumpsDirs()
+	allBackups, err := findAllBackups()
 	if err != nil {
 		return err
 	}
 
-	for _, d := range dirs {
-		_, err := getBackupTimestampFromDirName(d)
-		if err != nil {
-			return err
-		}
+	backupsToRetain, err := findBackupsToRetain(retentionPeriod, allBackups)
+	if err != nil {
+		return err
+	}
+
+	err = dropBackups(backupsToRetain)
+	if err != nil {
+		return err
 	}
 
 	return nil
 }
 
-func findDumpsDirs() ([]string, error) {
-	var dirs []string
+func dropBackups(ri []retainInfo) error {
+	for _, elem := range ri {
+		slog.Info("purge",
+			slog.String("msg", "rm -rf"),
+			slog.String("path", filepath.ToSlash(elem.path)),
+		)
+		err := os.RemoveAll(elem.absPath)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func getKeepCnt() int {
+	cfg := config.Cfg()
+	keep := cfg.Retention.KeepLast
+	if keep <= 0 {
+		return 0
+	}
+	return keep
+}
+
+func findBackupsToRetain(rp time.Duration, rl retainList) ([]retainInfo, error) {
+	var result []retainInfo
+	currentTime := time.Now()
+
+	for k, v := range rl {
+
+		// (oldest to newest)
+		sort.SliceStable(v, func(i, j int) bool {
+			dateI := v[i].datetime
+			dateJ := v[j].datetime
+			return dateI.Before(dateJ)
+		})
+
+		toDelete := len(v) - getKeepCnt()
+		if toDelete <= 0 {
+			slog.Info("purge",
+				slog.String("key", k),
+				slog.String("msg", "nothing to purge"),
+			)
+		} else {
+			for i := 0; i < toDelete; i++ {
+				elem := v[i]
+				info, err := os.Stat(elem.path)
+				if err != nil {
+					return nil, fmt.Errorf("error accessing folder %s: %v", elem.path, err)
+				}
+				if currentTime.Sub(info.ModTime()) > rp {
+					result = append(result, elem)
+				}
+			}
+		}
+
+	}
+
+	return result, nil
+}
+
+func findAllBackups() (retainList, error) {
+	result := make(map[string][]retainInfo)
+
+	backups, err := findDumpsDirs()
+	if err != nil {
+		return nil, err
+	}
+	for _, b := range backups {
+		key := fmt.Sprintf("%s-%s-%s", b.host, b.port, b.dbname)
+		result[key] = append(result[key], b)
+	}
+
+	return result, nil
+}
+
+func findDumpsDirs() ([]retainInfo, error) {
+	var dirs []retainInfo
 	cfg := config.Cfg()
 
-	// Use filepath.WalkDir to traverse the directory
 	err := filepath.WalkDir(cfg.Dest, func(path string, d os.DirEntry, err error) error {
 		if err != nil {
 			return fmt.Errorf("error accessing path %s: %w", path, err)
 		}
-		base := filepath.Base(path)
-		if d.IsDir() && naming.BackupDmpRegex.MatchString(base) {
-			dirs = append(dirs, path)
+		basename := filepath.Base(path)
+		if d.IsDir() && path != cfg.Dest && naming.BackupDmpRegex.MatchString(basename) {
+			ri, err := parseBackupInfo(path)
+			if err != nil {
+				return err
+			}
+			dirs = append(dirs, ri)
 		}
 		return nil
 	})
@@ -54,24 +154,35 @@ func findDumpsDirs() ([]string, error) {
 	return dirs, err
 }
 
-func getBackupTimestampFromDirName(path string) (time.Time, error) {
-	baseName := filepath.Base(path)
-
-	if !naming.BackupDmpRegex.MatchString(baseName) {
-		return time.Time{}, fmt.Errorf("not a backup dir: %s", baseName)
-	}
-
+func parseBackupInfo(path string) (retainInfo, error) {
 	// 20250217135009--localhost-5432--demo.dmp
 
-	submatch := naming.BackupDmpRegex.FindStringSubmatch(baseName)
+	absPath, err := filepath.Abs(path)
+	if err != nil {
+		return retainInfo{}, err
+	}
+
+	basename := filepath.Base(path)
+	notBackupDir := fmt.Errorf("not a backup dir: %s", filepath.ToSlash(path))
+	submatch := naming.BackupDmpRegex.FindStringSubmatch(basename)
+
 	if len(submatch) != 5 {
-		return time.Time{}, fmt.Errorf("not a backup dir: %s", baseName)
+		return retainInfo{}, notBackupDir
 	}
 
 	date, err := time.Parse(naming.TimestampLayout, submatch[1])
 	if err != nil {
-		return time.Time{}, fmt.Errorf("not a backup dir: %s", baseName)
+		return retainInfo{}, notBackupDir
 	}
 
-	return date, nil
+	return retainInfo{
+		absPath:  absPath,
+		path:     path,
+		basename: basename,
+
+		datetime: date,
+		host:     submatch[2],
+		port:     submatch[3],
+		dbname:   submatch[4],
+	}, nil
 }
