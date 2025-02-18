1c78c6ff9dc4562e0ccd55885cd959a26ee5d5ec
alexey.zh <mailbox.stdio@gmail.com>
2025-02-18 13:36:49 +0500

    upload - working with upload tasks

--- internal/remote/sync.go
+++ internal/remote/sync.go
@@ -1,5 +1,18 @@
 package remote
 
+import (
+	"fmt"
+	"log/slog"
+	"path/filepath"
+	"sync"
+
+	"gopgdump/config"
+	"gopgdump/internal/fio"
+	"gopgdump/internal/local"
+	"gopgdump/internal/naming"
+	"gopgdump/internal/remote/uploader"
+)
+
 type uploadTask struct {
 	localPath  string
 	remotePath string
@@ -10,4 +23,201 @@ func SyncLocalWithRemote() error {
 	_ = uploadSftp()
 	_ = uploadS3()
 	return nil
+}
+
+// common routine for all remotes
+
+func deleteOnRemote(u uploader.Uploader) error {
+	cfg := config.Cfg()
+	sftpConfig := cfg.Upload.Sftp
+
+	// get remote dirs
+	topLevelRemoteDirs, err := u.ListTopLevelDirs(sftpConfig.Dest, naming.BackupDmpRegex)
+	if err != nil {
+		return err
+	}
+
+	// get local dirs
+	topLevelLocalDirs, err := fio.ListTopLevelDirs(cfg.Dest, naming.BackupDmpRegex)
+	if err != nil {
+		return err
+	}
+	localIndex := map[string]bool{}
+	for _, f := range topLevelLocalDirs {
+		localIndex[f] = true
+	}
+
+	// remove dirs on remote, that are not present locally
+	for _, remoteDirName := range topLevelRemoteDirs {
+		if !localIndex[remoteDirName] {
+			err := u.DeleteAll(filepath.ToSlash(filepath.Join(sftpConfig.Dest, remoteDirName)))
+			if err != nil {
+				slog.Error("remote",
+					slog.String("action", "rm -rf"),
+					slog.String("storage", string(u.GetType())),
+					slog.String("remote-path", remoteDirName),
+					slog.String("err", err.Error()),
+				)
+			} else {
+				slog.Debug("remote",
+					slog.String("action", "rm -rf"),
+					slog.String("storage", string(u.GetType())),
+					slog.String("remote-path", remoteDirName),
+					slog.String("status", "ok"),
+				)
+			}
+		}
+	}
+
+	return nil
+}
+
+func getDest(u uploader.Uploader) string {
+	cfg := config.Cfg()
+	s3Config := cfg.Upload.S3
+	sftpConfig := cfg.Upload.Sftp
+
+	if u.GetType() == uploader.S3UploaderType {
+		if s3Config.Prefix != "" {
+			return s3Config.Prefix
+		}
+		// bucket-level
+		return ""
+	}
+
+	if u.GetType() == uploader.SftpUploaderType {
+		return sftpConfig.Dest
+	}
+
+	return ""
+}
+
+func getFilesToUpload(u uploader.Uploader) ([]uploadTask, error) {
+	cfg := config.Cfg()
+	dest := getDest(u)
+
+	// local and remote backups
+	remoteFiles, err := u.ListObjects(dest)
+	if err != nil {
+		return nil, err
+	}
+	// here should be ONLY files from *.dmp dirs, NOT *.dirty ones
+	localFiles, err := getLocalFiles()
+	if err != nil {
+		return nil, err
+	}
+
+	// search index
+	relativeMapLocal, err := makeRelativeMap(cfg.Dest, localFiles)
+	if err != nil {
+		return nil, err
+	}
+	relativeMapRemote, err := makeRelativeMap(dest, remoteFiles)
+	if err != nil {
+		return nil, err
+	}
+
+	filesToUploadOnRemote := []uploadTask{}
+	for localFile := range relativeMapLocal {
+		if !relativeMapRemote[localFile] {
+			// make actual paths from relatives (we compare relatives, but working with actual)
+			localFilePath := filepath.Join(cfg.Dest, localFile)
+
+			// dest may be empty for s3, when the prefix is not set, and all objects are stored at the bucket level
+			remoteFilePath := filepath.ToSlash(fmt.Sprintf("%s/%s", dest, localFile))
+			if dest == "" {
+				remoteFilePath = filepath.ToSlash(localFile)
+			}
+
+			filesToUploadOnRemote = append(filesToUploadOnRemote, uploadTask{
+				localPath:  localFilePath,
+				remotePath: remoteFilePath,
+			})
+		}
+	}
+
+	return filesToUploadOnRemote, nil
+}
+
+func uploadOnRemote(u uploader.Uploader) error {
+	// prepare tasks
+	filesToUploadOnRemote, err := getFilesToUpload(u)
+	if err != nil {
+		return err
+	}
+
+	// upload concurrently
+	workerCount := 8
+	uploadTasksCh := make(chan uploadTask, len(filesToUploadOnRemote))
+	var wg sync.WaitGroup
+	for i := 0; i < workerCount; i++ {
+		wg.Add(1)
+		go uploadWorker(i, u, uploadTasksCh, &wg)
+	}
+	for _, db := range filesToUploadOnRemote {
+		uploadTasksCh <- db
+	}
+	close(uploadTasksCh)
+	wg.Wait()
+
+	return nil
+}
+
+func uploadWorker(worker int, u uploader.Uploader, tasks <-chan uploadTask, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for t := range tasks {
+		err := u.Upload(t.localPath, t.remotePath)
+		if err != nil {
+			slog.Error("remote",
+				slog.String("action", "upload"),
+				slog.String("storage", string(u.GetType())),
+				slog.Int("worker", worker),
+				slog.String("local-path", filepath.ToSlash(t.localPath)),
+				slog.String("remote-path", filepath.ToSlash(t.remotePath)),
+				slog.String("err", err.Error()),
+			)
+		} else {
+			slog.Debug("remote",
+				slog.String("action", "upload"),
+				slog.String("storage", string(u.GetType())),
+				slog.Int("worker", worker),
+				slog.String("local-path", filepath.ToSlash(t.localPath)),
+				slog.String("remote-path", filepath.ToSlash(t.remotePath)),
+				slog.String("status", "ok"),
+			)
+		}
+	}
+}
+
+func makeRelativeMap(basepath string, input []string) (map[string]bool, error) {
+	basepath = filepath.ToSlash(basepath)
+	result := make(map[string]bool)
+	for _, f := range input {
+		f = filepath.ToSlash(f)
+		rel, err := filepath.Rel(basepath, f)
+		if err != nil {
+			return nil, err
+		}
+		result[rel] = true
+	}
+	return result, nil
+}
+
+func getLocalFiles() ([]string, error) {
+	backups, err := local.FindAllBackups()
+	if err != nil {
+		return nil, err
+	}
+	localFiles := []string{}
+	for _, v := range backups {
+		for _, b := range v {
+			allFilesInDir, err := fio.GetAllFilesInDir(b.Path)
+			if err != nil {
+				return nil, err
+			}
+			localFiles = append(localFiles, allFilesInDir...)
+		}
+	}
+	return localFiles, nil
 }
--- internal/remote/sync_s3.go
+++ internal/remote/sync_s3.go
@@ -1,11 +1,7 @@
 package remote
 
 import (
-	"fmt"
-
 	"gopgdump/internal/remote/uploader"
-
-	"gopgdump/internal/naming"
 
 	"gopgdump/config"
 )
@@ -17,21 +13,13 @@ func uploadS3() error {
 		return nil
 	}
 
-	s3Uploader, err := uploader.NewUploader(uploader.S3UploaderType, cfg.Upload)
+	u, err := uploader.NewUploader(uploader.S3UploaderType, cfg.Upload)
 	if err != nil {
 		return nil
 	}
 
-	path := s3Config.Bucket
-	if s3Config.Prefix != "" {
-		path = fmt.Sprintf("%s/%s", s3Config.Bucket, s3Config.Prefix)
-	}
-
-	dirs, err := s3Uploader.ListTopLevelDirs(path, naming.BackupDmpRegex)
-	if err != nil {
-		return err
-	}
-	fmt.Println(dirs)
+	_ = uploadOnRemote(u)
+	_ = deleteOnRemote(u)
 
 	return nil
 }
