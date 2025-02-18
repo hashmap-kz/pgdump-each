c0b420df728b84f4d41f0290ef79ad8123ce4044
alexey.zh <mailbox.stdio@gmail.com>
2025-02-18 10:49:02 +0500

    upload - clean code, sftp sync

--- internal/remote/sync.go
+++ internal/remote/sync.go
@@ -6,9 +6,15 @@ import (
 	"os"
 	"path/filepath"
 	"sort"
+	"sync"
 
 	"gopgdump/config"
 )
+
+type uploadTask struct {
+	localPath  string
+	remotePath string
+}
 
 func SyncLocalWithRemote() error {
 	return uploadSftp()
@@ -16,23 +22,22 @@ func SyncLocalWithRemote() error {
 
 func uploadSftp() error {
 	cfg := config.Cfg()
-	uploadIsEnabled := cfg.Upload.Sftp.Enable
-	if !uploadIsEnabled {
+	sftpConfig := cfg.Upload.Sftp
+	if !sftpConfig.Enable {
 		return nil
 	}
 
+	// init sftp client
 	sftpUploader, err := NewUploader(SftpUploaderType, cfg.Upload)
 	if err != nil {
 		return err
 	}
 
-	// remote backups
-	remoteFiles, err := sftpUploader.ListObjects(cfg.Upload.Sftp.Dest)
+	// local and remote backups
+	remoteFiles, err := sftpUploader.ListObjects(sftpConfig.Dest)
 	if err != nil {
 		return err
 	}
-
-	// local backups
 	localFiles, err := getAllFilesInDir(cfg.Dest)
 	if err != nil {
 		return err
@@ -43,56 +48,43 @@ func uploadSftp() error {
 	if err != nil {
 		return err
 	}
-	relativeMapRemote, err := makeRelativeMap(cfg.Upload.Sftp.Dest, remoteFiles)
+	relativeMapRemote, err := makeRelativeMap(sftpConfig.Dest, remoteFiles)
 	if err != nil {
 		return err
 	}
 
-	// upload on remote
-	for localFile := range relativeMapLocal {
-		if !relativeMapRemote[localFile] {
-			// make actual paths from relatives (we compare relatives, but working with actual)
-			localFilePath := filepath.Join(cfg.Dest, localFile)
-			remoteFilePath := filepath.ToSlash(fmt.Sprintf("%s/%s", cfg.Upload.Sftp.Dest, localFile))
+	uploadOnRemote(relativeMapLocal, relativeMapRemote, sftpUploader)
+	deleteOnRemote(relativeMapLocal, relativeMapRemote, sftpUploader)
+	return nil
+}
 
-			err := sftpUploader.Upload(localFilePath, remoteFilePath)
-			if err != nil {
-				slog.Error("remote",
-					slog.String("action", "upload"),
-					slog.String("remote-path", remoteFilePath),
-					slog.String("status", "err"),
-					slog.String("err", err.Error()),
-				)
-			} else {
-				slog.Debug("remote",
-					slog.String("action", "upload"),
-					slog.String("remote-path", remoteFilePath),
-					slog.String("status", "ok"),
-					slog.String("path", remoteFilePath),
-				)
-			}
-		}
-	}
-	// remove on remote
+func deleteOnRemote(
+	relativeMapLocal map[string]bool,
+	relativeMapRemote map[string]bool,
+	sftpUploader Uploader,
+) {
+	cfg := config.Cfg()
+	sftpConfig := cfg.Upload.Sftp
+
 	dirsToRemoveOnRemote := map[string]bool{}
 	for remoteFile := range relativeMapRemote {
 		if !relativeMapLocal[remoteFile] {
-			remotePathToRm := filepath.ToSlash(fmt.Sprintf("%s/%s", cfg.Upload.Sftp.Dest, remoteFile))
+			remotePathToRm := filepath.ToSlash(fmt.Sprintf("%s/%s", sftpConfig.Dest, remoteFile))
 			remoteDirToRm := filepath.ToSlash(filepath.Dir(remotePathToRm))
 			if remoteDirToRm != "." &&
 				remoteDirToRm != ".." &&
-				remoteDirToRm != filepath.ToSlash(cfg.Upload.Sftp.Dest) {
+				remoteDirToRm != filepath.ToSlash(sftpConfig.Dest) {
 				dirsToRemoveOnRemote[remoteDirToRm] = true
 			}
 		}
 	}
+
 	for k := range dirsToRemoveOnRemote {
 		err := sftpUploader.DeleteAll(k)
 		if err != nil {
 			slog.Error("remote",
 				slog.String("action", "rm -rf"),
 				slog.String("remote-path", k),
-				slog.String("status", "err"),
 				slog.String("err", err.Error()),
 			)
 		} else {
@@ -103,8 +95,66 @@ func uploadSftp() error {
 			)
 		}
 	}
+}
 
-	return nil
+func uploadOnRemote(
+	relativeMapLocal map[string]bool,
+	relativeMapRemote map[string]bool,
+	sftpUploader Uploader,
+) {
+	cfg := config.Cfg()
+	sftpConfig := cfg.Upload.Sftp
+
+	filesToUploadOnRemote := []uploadTask{}
+	for localFile := range relativeMapLocal {
+		if !relativeMapRemote[localFile] {
+			// make actual paths from relatives (we compare relatives, but working with actual)
+			localFilePath := filepath.Join(cfg.Dest, localFile)
+			remoteFilePath := filepath.ToSlash(fmt.Sprintf("%s/%s", sftpConfig.Dest, localFile))
+
+			filesToUploadOnRemote = append(filesToUploadOnRemote, uploadTask{
+				localPath:  localFilePath,
+				remotePath: remoteFilePath,
+			})
+		}
+	}
+
+	// upload concurrently
+	workerCount := 8
+	uploadTasksCh := make(chan uploadTask, len(filesToUploadOnRemote))
+	var wg sync.WaitGroup
+	for i := 0; i < workerCount; i++ {
+		wg.Add(1)
+		go uploadWorker(sftpUploader, uploadTasksCh, &wg)
+	}
+	for _, db := range filesToUploadOnRemote {
+		uploadTasksCh <- db
+	}
+	close(uploadTasksCh)
+	wg.Wait()
+}
+
+func uploadWorker(uploader Uploader, tasks <-chan uploadTask, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for t := range tasks {
+		err := uploader.Upload(t.localPath, t.remotePath)
+		if err != nil {
+			slog.Error("remote",
+				slog.String("action", "upload"),
+				slog.String("local-path", filepath.ToSlash(t.localPath)),
+				slog.String("remote-path", filepath.ToSlash(t.remotePath)),
+				slog.String("err", err.Error()),
+			)
+		} else {
+			slog.Debug("remote",
+				slog.String("action", "upload"),
+				slog.String("local-path", filepath.ToSlash(t.localPath)),
+				slog.String("remote-path", filepath.ToSlash(t.remotePath)),
+				slog.String("status", "ok"),
+			)
+		}
+	}
 }
 
 func makeRelativeMap(basepath string, input []string) (map[string]bool, error) {
