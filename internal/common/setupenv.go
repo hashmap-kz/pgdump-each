97b9928392162f5f9508086fe917574c145d22f5
alexey.zh <mailbox.stdio@gmail.com>
2025-03-31 11:12:36 +0500

    integ - waiting while pg_isready before attempting to dump

--- internal/common/setupenv.go
+++ internal/common/setupenv.go
@@ -3,15 +3,19 @@ package common
 import (
 	"context"
 	"fmt"
+	"log/slog"
 	"os"
 	"os/exec"
+	"time"
 
 	"github.com/jackc/pgconn"
 	"github.com/jackc/pgx/v5"
 )
 
-func SetupEnv(ctx context.Context, connStr string) error {
-	if err := validateConnStr(ctx, connStr); err != nil {
+const DefaultConnWaitTimeout = 30 * time.Second
+
+func SetupEnv(_ context.Context, connStr string) error {
+	if err := validateConnStr(connStr); err != nil {
 		return err
 	}
 	if err := setEnvFromConnStr(connStr); err != nil {
@@ -70,14 +74,29 @@ func setEnvFromConnStr(connStr string) error {
 	return nil
 }
 
-func validateConnStr(ctx context.Context, connStr string) error {
-	conn, err := pgx.Connect(ctx, connStr)
-	if err != nil {
-		return fmt.Errorf("failed to connect to PostgreSQL: %w", err)
+func validateConnStr(connStr string) error {
+	deadline := time.Now().Add(DefaultConnWaitTimeout)
+
+	for {
+		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+		defer cancel()
+
+		conn, err := pgx.Connect(ctx, connStr)
+		if err == nil {
+			if err := conn.Ping(ctx); err != nil {
+				conn.Close(ctx)
+				return fmt.Errorf("ping failed: %w", err)
+			}
+			slog.Info("pg_isready", slog.String("status", "ok"))
+			conn.Close(ctx)
+			return nil // Ready
+		}
+
+		if time.Now().After(deadline) {
+			return fmt.Errorf("PostgreSQL not ready after %s: %w", DefaultConnWaitTimeout, err)
+		}
+
+		slog.Info("pg_isready", slog.Any("err", err))
+		time.Sleep(1 * time.Second)
 	}
-	defer conn.Close(ctx)
-	if err := conn.Ping(ctx); err != nil {
-		return fmt.Errorf("ping failed: %w", err)
-	}
-	return nil
 }
